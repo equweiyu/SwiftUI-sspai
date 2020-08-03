79f1dea28989b77062dad4705eae284326b3985c
baitianwei <equweiyu@hotmail.com>
2020-08-03 17:58:08 +0800

    ViewModel

--- SwiftUI-sspai/ItemList.swift
+++ SwiftUI-sspai/ItemList.swift
@@ -7,79 +7,78 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ItemList: View {
-    @State var items: [ItemBean] = []
+
+    @ObservedObject var viewModel = ItemListViewModel()
+
     var body: some View {
         NavigationView {
-            List(0 ..< self.items.count, id: \.self) { index in
+            List(0 ..< self.viewModel.items.count, id: \.self) { index in
                 ZStack {
-
-                    ItemCell(item: self.items[index])
+                    ItemCell(item: self.viewModel.items[index])
                         .onAppear {
-                            self.fetchMoreItemsIfNeed(current: index)
+                            self.viewModel.fetchItemListMoreIfNeedPublisher.send(index)
                     }
-                    NavigationLink(destination: WebView(urlString: "https://sspai.com/post/\(self.items[index].id)" ).navigationBarTitle("", displayMode: .inline)
+                    NavigationLink(destination: WebView(urlString: "https://sspai.com/post/\(self.viewModel.items[index].id)" ).navigationBarTitle("", displayMode: .inline)
                         .edgesIgnoringSafeArea(Edge.Set.all)
                     ) {
                         EmptyView()
                     }
-
                 }
-            }.onAppear {
-                self.fetchItemList()
+            }
+            .onAppear {
+                self.viewModel.fetchItemListNewPublisher.send({}())
             }
             .navigationBarTitle("sspai")
             .navigationBarItems(trailing: Button(action: {
-                self.fetchItemList()
+                self.viewModel.fetchItemListNewPublisher.send(())
             }, label: {
                 Text("刷新")
             }))
         }
     }
 
-    func fetchItemList() {
-        let url = URL(string: "https://sspai.com/api/v1/article/index/page/get")!
-        URLSession.shared.dataTask(with: url) { (data, _, _) in
-            guard let data = data else {
-                // Error
-                return
-            }
-            guard let items = try? JSONDecoder().decode(NetworkResponse<[ItemBean]>.self, from: data).data else {
-                // Error
-                return
-            }
-            DispatchQueue.main.async {
-                self.items = items
-            }
-        }.resume()
-    }
-
-    func fetchMoreItemsIfNeed(current: Int) {
-        guard current == self.items.count - 1 else {
-            return
-        }
-        let url = URL(string: "https://sspai.com/api/v1/article/index/page/get?limit=10&offset=\(items.count)")!
-
-        URLSession.shared.dataTask(with: url) { (data, _, _) in
-            DispatchQueue.main.async {
-                guard let data = data else {
-                    // Error
-                    return
-                }
-                guard let items = try? JSONDecoder().decode(NetworkResponse<[ItemBean]>.self, from: data).data else {
-                    // Error
-                    return
-                }
-                self.items.append(contentsOf: items)
-            }
-
-        }.resume()
-    }
 }
 
 struct ItemList_Previews: PreviewProvider {
     static var previews: some View {
         ItemList()
+    }
+}
+
+class ItemListViewModel: ObservableObject {
+    private var cancellableSet: Set<AnyCancellable> = []
+    @Published var items: [ItemBean] = []
+    let fetchItemListNewPublisher = PassthroughSubject<Void, Never>()
+    let fetchItemListMoreIfNeedPublisher = PassthroughSubject<Int, Never>()
+
+    private let fetchItemList = { (url: URL) in
+        URLSession.shared.dataTaskPublisher(for: url)
+            .eraseToAnyPublisher()
+            .receive(on: DispatchQueue.main)
+            .map({ $0.data })
+            .decode(type: NetworkResponse<[ItemBean]>.self, decoder: JSONDecoder())
+            .map({ $0.data })
+    }
+
+    init() {
+
+        fetchItemListNewPublisher
+            .map({ URL(string: "https://sspai.com/api/v1/article/index/page/get")! })
+            .setFailureType(to: Error.self)
+            .flatMap(maxPublishers: .max(1), fetchItemList)
+            .replaceError(with: [])
+            .assign(to: \.items, on: self).store(in: &cancellableSet)
+
+        fetchItemListMoreIfNeedPublisher
+            .filter({ $0 == self.items.count - 1 })
+            .map({ _ in URL(string: "https://sspai.com/api/v1/article/index/page/get?limit=10&offset=\(self.items.count)")! })
+            .setFailureType(to: Error.self)
+            .flatMap(maxPublishers: .max(1), fetchItemList)
+            .replaceError(with: [])
+            .map({ self.items + $0 })
+            .assign(to: \.items, on: self).store(in: &cancellableSet)
     }
 }
