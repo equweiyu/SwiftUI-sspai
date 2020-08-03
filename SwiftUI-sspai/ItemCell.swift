e89ef7f80a3bb21022c53f163669c0fd39b03a99
baitianwei <equweiyu@hotmail.com>
2020-08-03 15:30:10 +0800

    KingfisherSwiftUI

--- SwiftUI-sspai/ItemCell.swift
+++ SwiftUI-sspai/ItemCell.swift
@@ -7,13 +7,15 @@
 //
 
 import SwiftUI
+import KingfisherSwiftUI
 
 struct ItemCell: View {
     let item: ItemBean
     var body: some View {
         VStack {
             GeometryReader { geometry in
-                NetWorkImage(url: URL(string: "https://cdn.sspai.com/\(self.item.banner)")!)
+                KFImage(URL(string: "https://cdn.sspai.com/\(self.item.banner)"))
+                    .resizable()
                     .scaledToFill()
                     .frame(width: geometry.size.width, height: geometry.size.width/2)
                     .clipShape(RoundedRectangle(cornerRadius: 16))
@@ -34,46 +36,5 @@ struct ItemCell: View {
 struct ItemCell_Previews: PreviewProvider {
     static var previews: some View {
         ItemCell(item: ItemBean.mockData())
-    }
-}
-
-struct NetWorkImage: View {
-    init(url: URL) {
-        self.imageLoader = Loader(url)
-    }
-
-    @ObservedObject private var imageLoader: Loader
-    var image: UIImage? {
-        imageLoader.data.flatMap(UIImage.init)
-    }
-
-    var body: some View {
-        VStack {
-            if image != nil {
-                Image(uiImage: image!)
-                    .resizable()
-            } else {
-                EmptyView()
-            }
-        }
-    }
-
-}
-
-final class Loader: ObservableObject {
-
-    var task: URLSessionDataTask!
-    @Published var data: Data?
-
-    init(_ url: URL) {
-        task = URLSession.shared.dataTask(with: url, completionHandler: { data, _, _ in
-            DispatchQueue.main.async {
-                self.data = data
-            }
-        })
-        task.resume()
-    }
-    deinit {
-        task.cancel()
     }
 }
